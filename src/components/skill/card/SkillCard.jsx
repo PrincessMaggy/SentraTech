6363532ddc7cc89581f5a5cbbcf35df00c86b121
Maggy <edoziemagdalene@gmail.com>
2026-01-20 14:37:54 +0100

    Change icons in skills section

--- src/components/skill/card/SkillCard.jsx
+++ src/components/skill/card/SkillCard.jsx
@@ -9,14 +9,28 @@ export default function SkillCard({ skill, i = 0 }) {
       data-fade-from="left"
       data-delay={delayFunction(i + 1)}
     >
-      <Image
-        width={48}
-        height={48}
-        style={{ width: "auto" }}
-        priority
-        src={skill.image}
-        alt="Skill Icon"
-      />
+      <div
+        style={{
+          padding: "10px",
+          backgroundColor: "var(--primary)",
+          borderRadius: "50%",
+          width: "50px",
+          height: "50px",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          marginBottom: "20px",
+        }}
+      >
+        <Image
+          width={26}
+          height={26}
+          style={{ width: "auto" }}
+          priority
+          src={skill.image}
+          alt="Skill Icon"
+        />
+      </div>
       <h5 className="title pb-10">{skill.name}</h5>
       <h4 className="percentage wc-counter">{skill.percentage}%</h4>
     </div>
